84bf57a114abfbed37d333df46499245f6cb19d6
N8Lance <nlance301@gmail.com>
2024-03-09 12:56:11 -0500

    input vidoes and fixed titles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,14 +9,11 @@ export default function Home() {
             <section class="shadow-pop-header">
             <section class="header">
                 <section class="head_top">
-                    <section class="head_top1">
-                        <section><img src={headQmark} width="250" height="150"/></section>
-                    </section>
                     <section class="head_top2">
-                        <h2>Andrew ARC Electric LLC.</h2>
+                        <p><strong>Andrew ARC<br/>Electric LLC.</strong></p>
                     </section>
                     <section class="head_top1">
-                        <section><h2>All wanted contant info</h2></section>
+                        <section><h2><b>Contact Me</b></h2><h3>Jonathan Andrews</h3><h3>Phone: 443-350-8335</h3><h3>Email: aarchelectric@gmail.com</h3></section>
                     </section>
                 </section>
                 <section class="head_bot">    
@@ -42,19 +39,21 @@ export default function Home() {
                     </section>
                 </section>
             </section>
+            <section class="shadow-pop-header">
             <section class="learn">
                 <section class="learn_title"><h2>Learn</h2></section>
                 <section class="learn_contents">
                     <section class="learn_vidoes">
-                        <section class="video_content"><img src={Qmark} width="500" height="275"/></section>
-                        <section class="video_content"><img src={Qmark} width="500" height="275"/></section>
+                        <section class="video_content"><iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/xd57G5xIjl4?si=TsRfFK2NrvcMfyGd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></section>
+                        <section class="video_content"><iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/koRS2G5o3ek?si=l2iodu8OmCYhHosK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></section>
                     </section>
                     <section class="learn_questions">
+                        <section class="q"><h2>What is span?</h2></section>
                         <section class="q"><h2>What is a Tesla Powerwall?</h2></section>
-                        <section class="q"><h2>What is Span Certification?</h2></section>
                         <section class="q"><h2>How does this all work?</h2></section>
                     </section>
                 </section>
+            </section>
             </section>
             <section class="future_title"><h2>Future</h2></section>
             <section class="future">
@@ -65,18 +64,16 @@ export default function Home() {
                     <h2>What do we want to add here?</h2>
                 </section>
             </section>
+            <section class="shadow-pop-header">
             <section class="review">
                 <section class="review_title"><h2>Reviews</h2></section>
                 <section class="review_content"><h2>Need python backend</h2></section>
             </section>
+            </section>
             <section class="reach_out">
                 <section class="b1_box">
-                    <h2>Who refered you?</h2>
-                    <button class="b1" src="google.com">Fill out our referral<br></br>sheet!!</button>
-                </section>
-                <section class="b1_box">
                     <h2>Looking for a quote?</h2>
-                    <button class="b1" src="google.com">Get one now!!</button>
+                    <button class="b1"><a href="mailto:aarchelectric@gmailcom">aarchelectric@gmail.com</a></button>
                 </section>
             </section>
         </section>
