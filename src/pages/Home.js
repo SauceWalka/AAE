694e56a0b65bfd9be94e0e2cd5e945055df0894a
N8Lance <nlance301@gmail.com>
2024-03-15 16:42:00 -0400

    lots of stuff and Slider

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import "./css/home.css"
 import Footer from "./Footer.js"
-import Qmark from "./images/Qmark.jpg"
+import TCert from "./images/TeslaCert.PNG"
+import Slider from "./Slider.js"
 
 export default function Home() {
     return <div>
@@ -9,20 +10,17 @@ export default function Home() {
             <section class="header">
                 <section class="head_top">
                     <section class="head_top2">
-                        <p><strong>Andrew ARC<br/>Electric LLC.</strong></p>
+                        <section classname="head_top2_title">
+                            <p><strong>Andrews Ark<br/>Electric LLC.</strong></p>
+                        </section>
+                        <section class="head_bot_bot">
+                            <section><img src={TCert} width="140" height="90"/></section>
+                            <section><h3>Span: XXXXXXXXX</h3>
+                        </section>
+                    </section>
                     </section>
                     <section class="head_top1">
                         <section><h2><b>Contact Me</b></h2><h3>Jonathan Andrews</h3><h3>Phone: 443-350-8335</h3><h3>Email: aarchelectric@gmail.com</h3></section>
-                    </section>
-                </section>
-                <section class="head_bot">    
-                    <section class="head_bot_header">
-                        <h2>Our Certifications</h2>
-                    </section>    
-                    <section class="head_bot_bot">
-                        <section><h2>Tesla: XXXXXXXXX</h2></section>
-                        <section><h2>Span: XXXXXXXXX</h2></section>
-                        <section><h2>Electric: XXXXXXXXX</h2></section>
                     </section>
                 </section>
             </section>
@@ -32,9 +30,9 @@ export default function Home() {
             </section>
             <section class="projects">
                 <section class="outer_box">
-                    <section class="img_slider"><img src={Qmark} width="800" height="400"/></section>
+                    <section class="img_slider"><Slider /></section>
                     <section class="inner_box">
-                        <h2>Description for your projects</h2>
+                        <h2>Here are some of the projects that Andrews Ark Electric has completed recently</h2>
                     </section>
                 </section>
             </section>
@@ -43,13 +41,14 @@ export default function Home() {
                 <section class="learn_title"><h2>Learn</h2></section>
                 <section class="learn_contents">
                     <section class="learn_vidoes">
-                        <section class="video_content"><iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/xd57G5xIjl4?si=TsRfFK2NrvcMfyGd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></section>
-                        <section class="video_content"><iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/koRS2G5o3ek?si=l2iodu8OmCYhHosK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></section>
+                        <section><iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/xd57G5xIjl4?si=TsRfFK2NrvcMfyGd" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></section>
+                        <section><iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/koRS2G5o3ek?si=l2iodu8OmCYhHosK" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></section>
+                        <section><iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/AigKe_7uWUw?si=gOdxfMpa_Tm8GGZL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></section>
                     </section>
                     <section class="learn_questions">
                         <section class="q"><h2>What is span?</h2></section>
-                        <section class="q"><h2>What is a Tesla Powerwall?</h2></section>
-                        <section class="q"><h2>How does this all work?</h2></section>
+                        <section class="q"><h2>What is a Tesla<br/> Powerwall?</h2></section>
+                        <section class="q"><h2>What is Tesla's<br/> wall connector?</h2></section>
                     </section>
                 </section>
             </section>
---
+++ src/pages/slider-data.js
@@ -0,0 +1,22 @@
+export const sliderData = [
+    {
+      image: "https://live.staticflickr.com/65535/53589905763_8a34229aca_k.jpg",
+      heading: "Slide One",
+      desc: "This is the description of slide one Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+    },
+    {
+      image: "https://live.staticflickr.com/65535/53588889762_f4c7b401bc_k.jpg",
+      heading: "Slide Two",
+      desc: "This is the description of slide two Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+    },
+    {
+      image: "https://live.staticflickr.com/65535/53590027439_a0c1bd09e1_k.jpg",
+      heading: "Slide Three",
+      desc: "This is the description of slide three Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+    },
+    {
+      image: "https://live.staticflickr.com/65535/53589974408_88e6a88ddf_k.jpg",
+      heading: "Slide Four",
+      desc: "This is the description of slide three Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+    },
+  ];
