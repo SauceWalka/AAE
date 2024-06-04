5e4100d89f0fc46c885dc9ac439cfd7f75fcaf79
N8Lance <nlance301@gmail.com>
2024-06-03 21:09:49 -0400

    Mobile padding and pictures added to slider

--- src/pages/slider-data.js
+++ src/pages/slider-data.js
@@ -32,21 +32,61 @@ export const sliderData = [
     {
       image: "https://live.staticflickr.com/65535/53709027620_fb7d620525_k.jpg",
       heading: "Slide Seven",
-      desc: "This is the description of slide six Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+      desc: "This is the description of slide seven Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
      },
      {
       image: "https://live.staticflickr.com/65535/53708807478_a6fb782e65_k.jpg",
       heading: "Slide Eight",
-      desc: "This is the description of slide six Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+      desc: "This is the description of slide eight Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
      },
      {
       image: "https://live.staticflickr.com/65535/53708807413_9fd4b85e67_k.jpg",
       heading: "Slide Nine",
-      desc: "This is the description of slide six Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+      desc: "This is the description of slide nine Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
      },
      {
       image: "https://live.staticflickr.com/65535/53710497602_206a10683c_k.jpg",
       heading: "Slide Ten",
-      desc: "This is the description of slide six Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+      desc: "This is the description of slide ten Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+     },
+     {
+      image: "https://live.staticflickr.com/65535/53755480602_47cbb52d0e.jpg",
+      heading: "Slide Eleven",
+      desc: "This is the description of slide eleven Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+     },
+     {
+      image: "https://live.staticflickr.com/65535/53756802860_09911ab397.jpg",
+      heading: "Slide Twelve",
+      desc: "This is the description of slide twelve Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+     },
+     {
+      image: "https://live.staticflickr.com/65535/53755480657_6a66a3614e.jpg",
+      heading: "Slide Thirteen",
+      desc: "This is the description of slide thirteen Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+     },
+     {
+      image: "https://live.staticflickr.com/65535/53756802990_157f746355.jpg",
+      heading: "Slide Fourteen",
+      desc: "This is the description of slide fourteen Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+     },
+     {
+      image: "https://live.staticflickr.com/65535/53756390126_b26e56585d.jpg",
+      heading: "Slide Fifteen",
+      desc: "This is the description of slide fifteen Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+     },
+     {
+      image: "https://live.staticflickr.com/65535/53756802880_71d89ab9df.jpg",
+      heading: "Slide Sixteen",
+      desc: "This is the description of slide sixteen Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+     },
+     {
+      image: "https://live.staticflickr.com/65535/53756802940_f17b21b2f7.jpg",
+      heading: "Slide Seventeen",
+      desc: "This is the description of slide seventeen Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
+     },
+     {
+      image: "https://live.staticflickr.com/65535/53756390166_f0a2b9b98b.jpg",
+      heading: "Slide Eighteen",
+      desc: "This is the description of slide eighteen Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi quos quas, voluptatum nesciunt illum exercitationem.",
      }
   ];
